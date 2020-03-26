444cadf68ec4d00bfed1a9bdd2989dc8dcaf7663
Theresa Mammarella <Theresa.T.Mammarella@ibm.com>
2020-03-26 12:01:39 -0700

    Add exit agent exclusive vm access flags
    Signed-off-by: Theresa Mammarella <Theresa.T.Mammarella@ibm.com>

--- runtime/rasdump/dmpagent.c
+++ runtime/rasdump/dmpagent.c
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 1991, 2019 IBM Corp. and others
+ * Copyright (c) 1991, 2020 IBM Corp. and others
  *
  * This program and the accompanying materials are made available under
  * the terms of the Eclipse Public License 2.0 which accompanies this
@@ -437,7 +437,7 @@ static const J9RASdumpSpec rasDumpSpecs[] =
 		  NULL,
 		  NULL,
 		  0,
-		  J9RAS_DUMP_DO_SUSPEND_OTHER_DUMPS,
+		  J9RAS_DUMP_DO_EXCLUSIVE_VM_ACCESS,
 		  NULL }
 	}
 };
