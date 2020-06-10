63cc0a447d62161a5d79b435ca4f8f8973c9113e
Theresa Mammarella <Theresa.T.Mammarella@ibm.com>
2020-06-10 15:38:49 -0700

    mer

--- runtime/rasdump/trigger.c
+++ runtime/rasdump/trigger.c
@@ -547,6 +547,7 @@ prepareForDump(struct J9JavaVM *vm, struct J9RASdumpAgent *agent, struct J9RASdu
 	RasGlobalStorage * j9ras = (RasGlobalStorage *)vm->j9rasGlobalStorage;
 	UtInterface * uteInterface = (UtInterface *)(j9ras ? j9ras->utIntf : NULL);
 	BOOLEAN exclusiveHeld = J9_XACCESS_NONE != vm->exclusiveAccessState;
+	BOOLEAN acquireVMAccessAfterWait = FALSE;
 
 	/* Is trace running? */
 	if( uteInterface && uteInterface->server ) {
@@ -555,9 +556,13 @@ prepareForDump(struct J9JavaVM *vm, struct J9RASdumpAgent *agent, struct J9RASdu
 		newState |= J9RAS_DUMP_TRACE_DISABLED;
 	}
 
-	/* Release vm access until this thread has the dumpKey and is ready to run. This will allow 
-	 * other threads to obtain exclusiveVMAccess in the meantime. */
-	vm->internalVMFunctions->internalReleaseVMAccess(vmThread);
+	/* Release vm access until this thread has the dumpKey and is ready to run. This will allow other threads to obtain exclusiveVMAccess in the meantime. */
+	if (vmThread) {
+		if (vmThread->publicFlags & J9_PUBLIC_FLAGS_VM_ACCESS) {
+			vm->internalVMFunctions->internalReleaseVMAccess(vmThread);
+			acquireVMAccessAfterWait = TRUE;
+		}
+	}
 
 	/*
 	 * The following actions are considered safe to call during a crash situation...
@@ -567,7 +572,7 @@ prepareForDump(struct J9JavaVM *vm, struct J9RASdumpAgent *agent, struct J9RASdu
 	 * serial dump lock, see below. This allows the first failing thread to complete its dumps and exit the VM, reducing
 	 * the number of dumps written and out-time if multiple threads crash.
 	 */
-	if (0 != (context->eventFlags & (J9RAS_DUMP_ON_GP_FAULT | J9RAS_DUMP_ON_ABORT_SIGNAL | J9RAS_DUMP_ON_TRACE_ASSERT))) {
+	if (J9_ARE_ANY_BITS_SET(context->eventFlags, J9RAS_DUMP_ON_GP_FAULT | J9RAS_DUMP_ON_ABORT_SIGNAL | J9RAS_DUMP_ON_TRACE_ASSERT)) {
 		compareAndSwapUDATA(&rasDumpFirstThread, 0, dumpKey);
 	}
 
@@ -577,7 +582,7 @@ prepareForDump(struct J9JavaVM *vm, struct J9RASdumpAgent *agent, struct J9RASdu
 		UDATA newKey = 0;
 
 		/* Grab the dump lock? */
-		if (agent->requestMask & J9RAS_DUMP_DO_SUSPEND_OTHER_DUMPS) {
+		if (J9_ARE_ANY_BITS_SET(agent->requestMask, J9RAS_DUMP_DO_SUSPEND_OTHER_DUMPS)) {
 			newState |= J9RAS_DUMP_GOT_LOCK;
 			newKey = dumpKey;
 		}
@@ -593,9 +598,11 @@ prepareForDump(struct J9JavaVM *vm, struct J9RASdumpAgent *agent, struct J9RASdu
 		}
 	}
 
-	vm->internalVMFunctions->internalAcquireVMAccess(vmThread);
+	if (acquireVMAccessAfterWait) {
+		vm->internalVMFunctions->internalAcquireVMAccess(vmThread);
+	}
 
-	if ((context->eventFlags & (J9RAS_DUMP_ON_GP_FAULT | J9RAS_DUMP_ON_ABORT_SIGNAL | J9RAS_DUMP_ON_TRACE_ASSERT)) == 0) {
+	if (J9_ARE_NO_BITS_SET(context->eventFlags, J9RAS_DUMP_ON_GP_FAULT | J9RAS_DUMP_ON_ABORT_SIGNAL | J9RAS_DUMP_ON_TRACE_ASSERT)) {
 		/*
 		 * The following actions may deadlock, so don't use them
 		 * if this is a crash situation or a trace assertion.
