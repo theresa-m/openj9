cd804b8abf6ea2d4f0a4121f6552926b5be17615
Theresa Mammarella <Theresa.T.Mammarella@ibm.com>
2025-06-05 11:45:34 -0400

    prints for takeVirtualThreadListToUnblock

--- runtime/vm/ContinuationHelpers.cpp
+++ runtime/vm/ContinuationHelpers.cpp
@@ -1192,6 +1192,7 @@ restart:
 				J9ObjectMonitor *syncObjectMonitor = current->objectWaitMonitor;
 				U_32 state = J9VMJAVALANGVIRTUALTHREAD_STATE(currentThread, current->vthread);
 				next = current->nextWaitingContinuation;
+				printf("Process thread: %p om: %p state: %u, count %u\n", current, syncObjectMonitor, state, syncObjectMonitor->platformThreadWaitCount);
 
 				/* Check if the blocking monitor has platform thread waiting,
 				 * which doesn't notify the unblocker when exiting monitor.
