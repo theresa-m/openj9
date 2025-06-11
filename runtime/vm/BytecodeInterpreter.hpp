7353b81288cc41f36d6cb47bf08bab4198602804
Theresa Mammarella <Theresa.T.Mammarella@ibm.com>
2025-06-11 10:40:03 -0400

    print return from object wait result

--- runtime/vm/BytecodeInterpreter.hpp
+++ runtime/vm/BytecodeInterpreter.hpp
@@ -5844,10 +5844,12 @@ ffi_OOM:
 
 				/* Only throw an exception if the virtual thread has not been notified. */
 				if (interrupted	&& !notified) {
+					printf("interrupt %p\n", continuation);
 					setCurrentException(_currentThread, J9VMCONSTANTPOOL_JAVALANGINTERRUPTEDEXCEPTION, NULL);
 					VMStructHasBeenUpdated(REGISTER_ARGS);
 					rc = GOTO_THROW_CURRENT_EXCEPTION;
 				} else {
+					printf("ret %p\n", continuation);
 					restoreInternalNativeStackFrame(REGISTER_ARGS);
 					updateVMStruct(REGISTER_ARGS);
 					returnVoidFromINL(REGISTER_ARGS, 4);
